1bdfe9b3d6558a629d012ff512ff9a2b4613fc5a
hi3816 <99782819+hi3816@users.noreply.github.com>
2025-04-26 18:45:29 +0900

    fix : 라우터 문제

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  trailingSlash: true,
+  trailingSlash: false, // 기본값
 };
 
 export default nextConfig;
